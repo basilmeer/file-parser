87093bd64cf74480766add7815f902fa5a66c3d4
Basil Meer <basilmeer@gmail.com>
2017-07-23 11:39:46 +0500

    Rewrote some comments and changed the example paths

--- filesparser.py
+++ filesparser.py
@@ -1,13 +1,13 @@
 # Parses filenames in the specified directory, saves them to a CSV file
 import os, csv, sys
 
-# Change the path of the file to save in the CSV data
-f = open("K:/Programming/Python/names.csv",'r+', newline='', encoding="utf-8")
+# Path where the .csv is saved e.g. C:/foobar.csv
+f = open("/path/here",'r+', newline='', encoding="utf-8")
 # Instantiates the CSV Writer
 w = csv.writer(f)
 # Inserts column names in the first row
 w.writerow(('FileName', 'DirName'))
-# Write the path of the parent directory to scrap the data out from
+# Path to parse files from
 for path, dirs, files in os.walk("I:/"):
     for filename in files:
         dir_path = os.path.dirname(os.path.abspath(filename))
